037db40c04514828803fb672db3f3c41a7c935f9
chhayseavpav <chhayseavpav@gmail.com>
2023-03-22 07:22:25 +0700

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-800 px-6 py-4">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-wrap justify-between items-center">
+          <div className="flex-shrink-0">
+            <Link to="/" className="text-white font-bold text-xl mr-10">
+              KH Store
+            </Link>
+          </div>
+          <div className="flex-grow md:flex md:items-center">
+            <div className="md:flex-grow">
+              <Link
+                to="/"
+                className="block mt-4 md:inline-block md:mt-0 text-gray-500 hover:text-white mr-10"
+              >
+                Home
+              </Link>
+              <Link
+                to="/about"
+                className="block mt-4 md:inline-block md:mt-0 text-gray-500 hover:text-white mr-10"
+              >
+                About
+              </Link>
+              <Link
+                to="/contact"
+                className="block mt-4 md:inline-block md:mt-0 text-gray-500 hover:text-white mr-10"
+              >
+                Contact
+              </Link>
+            </div>
+            <div className="mt-4 md:mt-0">
+              <p className="text-gray-500">
+                Phone: <a href="#!">010 777 999</a>
+              </p>
+              <p className="text-gray-500">
+                Email: <a href="#!">khstore@gmail.com</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
