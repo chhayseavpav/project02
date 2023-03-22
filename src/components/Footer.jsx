e3057a4d85a21f85d262d7edf5e2912626a8d94f
chhayseavpav <chhayseavpav@gmail.com>
2023-03-22 18:09:09 +0700

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 px-6 py-4">
-      <div className="max-w-7xl mx-auto">
+    <footer className="bg-gray-800 py-4">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-wrap justify-between items-center">
           <div className="flex-shrink-0">
             <Link to="/" className="text-white font-bold text-xl mr-10">
