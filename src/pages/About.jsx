82ec9c6942fc53d71c23ffd61b9a1fc9da80e4a0
chhayseavpav <chhayseavpav@gmail.com>
2023-03-23 16:08:11 +0700

    Updata about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function About() {
-  return <div className="min-h-screen bg-gray-100">About</div>;
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="py-12">
+          <h1 className="text-3xl font-bold text-gray-900 mb-4">About Us</h1>
+          <p className="text-gray-700">
+            KH Store provides a great selection of products in various cities in
+            Cambodia such as Phnom Penh, Siem Reap, Battambang and many more.
+            Discover our prodeucts through the website. At KH Store, we deliver
+            happiness through products, groceries and everyday essentials. How?
+            By making ordering easy, fast and fun.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
