7fa44e08f99332e540ae5e7696362626c8203214
chhayseavpav <chhayseavpav@gmail.com>
2023-03-23 15:57:13 +0700

    Updata nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -45,7 +45,9 @@ function Nav() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <h1 className="text-white font-bold text-lg">KH Store</h1>
+                  <Link to="/" className="text-white font-bold text-lg">
+                    KH Store
+                  </Link>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
