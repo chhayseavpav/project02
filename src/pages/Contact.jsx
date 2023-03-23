311c199d89ec81c3f4604884c9c8d6ab987c81ba
chhayseavpav <chhayseavpav@gmail.com>
2023-03-23 16:08:55 +0700

    Updata contact  page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Contact() {
-  return <div className="min-h-screen bg-gray-100">Contact</div>;
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="py-12">
+          <h1 className="text-3xl font-bold text-gray-900 mb-4">Contact Us</h1>
+          <p>Email: khstore@gmail.com</p>
+          <p>Phone: 010 777 999</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
